732c4963348c5250b76caf7eda22633d4a5c8753
qingwangrh <qinwang@redhat.com>
2024-11-05 05:47:00 -0500

    Remove account info
    Signed-off-by: qingwangrh <qinwang@redhat.com>

--- ocp_deploy/ocp_install.sh
+++ ocp_deploy/ocp_install.sh
@@ -4,34 +4,36 @@
 
 # echo "${BASH_SOURCE}"
 
-WORK_PATH=$(dirname "${BASH_SOURCE[0]}")
+work_path=$(dirname "${BASH_SOURCE[0]}")
 
-echo "Work path: $WORK_PATH"
+echo "Work path: $work_path"
 
-usage="$0 <-u user> <-p password> [-f]
--u|-p:  user name | passwork for rhsm repo
--f   : force build public key, optional.
+usage="$0 <-u user> <-p password> [-c config file path] [-f]
+-u|-p:  user name | password for rhsm repo
+-f   : Flag of force build public key, optional.
+-c   : The config_${USER}.sh and pull_secret.json file path
 "
 
-SCRIPT_PATH=/home/dev-scripts
+script_path=/home/dev-scripts
 
-USER=
-PASSWORD=
-FORCE_FLAG=
+user=
+password=
+force=
 
 ocp_config_env() {
   mkdir -p /home/ocp/
 }
 
 ocp_config_repo() {
+  echo "$#"
   dnf install -y git make docker wget jq dnsmasq
   dnf install -y NetworkManager-initscripts-updown
   dnf install -y go-toolset net-tools bridge-utils
   dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
   dnf install -y ansible
   #You need set your own user/password
-  if (($# < 2)); then
-    subscription-manager register --serverurl=subscription.rhsm.stage.redhat.com:443/subscription --baseurl=https://cdn.redhat.com --username=hawkularqe --password=hawkularqe --auto-attach
+  if (($# > 1)); then
+    subscription-manager register --serverurl=subscription.rhsm.stage.redhat.com:443/subscription --baseurl=https://cdn.redhat.com --username=$1 --password=$2 --auto-attach
   else
     echo "Skip subscription-manager"
   fi
@@ -43,13 +45,14 @@ ocp_config_repo() {
 
 ocp_config_script() {
 
-  if [ -e ${SCRIPT_PATH}/config_script_end ]; then
+  if [ -e ${script_path}/config_script_end ]; then
     echo "Exist dev-scripts"
   else
-    git clone https://github.com/openshift-metal3/dev-scripts.git ${SCRIPT_PATH}
-    cp $WORK_PATH/config_root.sh ${SCRIPT_PATH}/
-    cp $WORK_PATH/pull_secret.json ${SCRIPT_PATH}/
-    cp $WORK_PATH/pull_secret.json ${HOME}/
+    git clone https://github.com/openshift-metal3/dev-scripts.git ${script_path}
+    local filepath=${1:-$work_path}
+    cp $filepath/config_${USER}.sh ${script_path}/
+    cp $filepath/pull_secret.json ${script_path}/
+    cp $filepath/pull_secret.json ${HOME}/
     touch /home/dev-scripts/config_script_end
   fi
 }
@@ -68,15 +71,13 @@ ocp_config_pubkey() {
 
 ocp_config_setup() {
   ocp_config_env
-  ocp_config_repo $USER $PASSWORD
-  ocp_config_script
-  ocp_config_pubkey $FORCE_FLAG
-  cd ${SCRIPT_PATH}
-  echo "Please copy your pull_secret.json into  ${SCRIPT_PATH}"
+  ocp_config_repo $user $password
+  ocp_config_script $confpath
+  ocp_config_pubkey $force
+  cd ${script_path}
+  echo "Please copy your pull_secret.json into  ${script_path}"
 
 }
-
-
 
 while getopts "fhu:p:" opt; do
   case $opt in
@@ -85,13 +86,16 @@ while getopts "fhu:p:" opt; do
     exit 0
     ;;
   u)
-    USER="$OPTARG"
+    user="$OPTARG"
     ;;
   f)
-    FORCE_FLAG=1
+    force=1
     ;;
   p)
-    PASSWORD="$OPTARG"
+    password="$OPTARG"
+    ;;
+  c)
+    confpath="$OPTARG"
     ;;
   ? | *)
     echo -e "$usage"
@@ -103,7 +107,13 @@ done
 
 # You may comment if want to call function only
 if [ "$0" != "-bash" ]; then
+  if [ "user" == "" ] || [ "$password" == "" ]; then
+    echo "Please set user and password "
+    echo -e "$usage"
+    exit 1
+  fi
   ocp_config_setup
 else
+  # test function only 
   echo "Please run ocp_config_setup "
 fi
